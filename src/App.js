c6615d1d44787d1fb25327f3dd9fe4334e5507c0
Soham Dutta <duttasoham2222@gmail.com>
2024-05-27 21:25:22 +0530

    Random feature added

--- src/App.js
+++ src/App.js
@@ -62,20 +62,6 @@ function App() {
   );
 }
 
-// function App() {
-//   //focus Input
-//   const inputEl = useRef(null);
-//   const onButtonClick = () => {
-//     // `current` points to the mounted text input element
-//     inputEl.current.focus();
-//   };
-//   return (
-//     <>
-//       <input ref={inputEl} type="text" />
-//       <button onClick={onButtonClick}>Focus the input</button>
-//     </>
-//   );
-// }
 
 // function App() {
 //   function handleClick(){
